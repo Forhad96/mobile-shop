51c25e955d0af4934a6bb53aa19432aa7b3dff12
Forhad96 <forhadairdrop@gmail.com>
2023-08-30 12:49:19 +0600

    first commit

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-const loadData = async (searchText = 'iphone',isShowAll) => {
+const loadData = async (searchText = "iphone", isShowAll) => {
   const res = await fetch(
     ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
   // console.log(phones);
-  displayData(phones,isShowAll);
+  displayData(phones, isShowAll);
 };
 
-const displayData = (phones,isShowAll) => {
+const displayData = (phones, isShowAll) => {
   const phonesContainer = document.getElementById("phones-container");
-  const showAll =document.getElementById('show-all')
+  const showAll = document.getElementById("show-all");
   // console.log(showAll);
 
-  phonesContainer.innerHTML = ''
+  phonesContainer.innerHTML = "";
 
-  if(phones.length > 15){
-    showAll.classList.remove('hidden')
-  }else{
-    showAll.classList.add('hidden')
+  if (phones.length > 15) {
+    showAll.classList.remove("hidden");
+  } else {
+    showAll.classList.add("hidden");
   }
-console.log(phones.length);
-  if(!isShowAll){
-    phones = phones.slice(0,9)
+  console.log(phones.length);
+  if (!isShowAll) {
+    phones = phones.slice(0, 9);
   }
 
-  
   phones.forEach((phones) => {
     const phoneCard = document.createElement("div");
     phoneCard.classList = "card bg-base shadow-xl";
@@ -37,43 +36,47 @@ console.log(phones.length);
         <h2 class="card-title">${phones.phone_name}</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
+          <button onclick = "buyNowHandler()" class="btn btn-primary">Buy Now</button>
+          <button class="btn btn-accent">Show Details</button>
         </div>
         
         `;
     phonesContainer.appendChild(phoneCard);
   });
-  loadingSnipper(false)
-
+  loadingSnipper(false);
 };
 
-
 // search handler function
-const searchHandler = async(isShowAll) =>{
-  const searchField = document.getElementById('search-field');
+const searchHandler = async (isShowAll) => {
+  const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   // console.log('button clicked',searchText);
-  loadData(searchText,isShowAll);
-  loadingSnipper(true)
-}
+  loadData(searchText, isShowAll);
+  loadingSnipper(true);
+};
 
 // Loading Snipper function
-const loadingSnipper = (isLoading) =>{
-  const loadingContainer = document.getElementById('loading-spinner');
-  if(isLoading){
-    loadingContainer.classList.remove('hidden')
+const loadingSnipper = (isLoading) => {
+  const loadingContainer = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingContainer.classList.remove("hidden");
+  } else {
+    loadingContainer.classList.add("hidden");
   }
-  else{
-    loadingContainer.classList.add('hidden')
-  }
+};
+
+// search show item handler
+const showAllHandler = () => {
+  searchHandler(true);
+};
+
+// Buy Now
+const buyNowHandler = () => {
+  console.log('clickd')
 }
 
-      // search show item handler
-      const showAllHandler = () =>{
-        // console.log('clicekd')
-        searchHandler(true)
-      }
+
+
 
 
 loadData();
-
